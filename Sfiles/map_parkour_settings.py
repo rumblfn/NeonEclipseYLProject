1f1c231680f07af85e9cc63ae29d81953c3bf62a
aaalace <tyj5resd@gmail.com>
2022-01-02 18:40:53 +0300

    economy realized

--- Sfiles/map_parkour_settings.py
+++ Sfiles/map_parkour_settings.py
@@ -2,18 +2,18 @@ import pygame
 
 level_parkour_map = [  # 18
     'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                                                00000000   G   00000000                                             XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXKGGGGGGX   0   0                                       0000   0000                                         0              GXXШ     a XXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXG G G GX   0   0                                       0000   0000                                         0              KXXШ     a XXXXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXGWWWWWW XXXXXXXXX            0                  0                 Z 0 0 V                 0                 0             Y       uXXXXXXXXXa к ЙXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   a                                            0   00000 00     00 00000   0                                      0      GXXXЩ    г aXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaXXXX                0      0      0                     0   0                     0      0      0                    GXXXXXXXXXXXa ц УXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaXXXXX                                                    0 0        DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD   GXXXXК     у aXXXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   a                                            0   00000 00     00 00000   0                                      0       XXXЩ    г aXXXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaXXXX                0      0      0                     0   0                     0      0      0                     XXXXXXXXXXXa ц УXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaXXXXX                                                    0 0        DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD    XXXXК     у aXXXXXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXG a  XXXXXaXXXXX                                                    0 0         d D   D   D   D   D   D   D   D   D        D XXXXXXXXXXXXXXXa е НXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXXXaX  XXXXAXXXXX                                                    0 0         d D   D   g   D   g   D   D   D   D        D XXXXXXЦ      н aXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXaXX      XXXXX                                                    0 0         d g   D       D           D   g   D        D XXXXXXXXXXXXXXXa щ ЕXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXX                               00000000G00000000                   d g           D       g           g        D XXXIXKGGGGGGGй aXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXu P                               0    0    0          0L0         d g   g   D   g   D       g                D XXXXXXXXXXXXXXXa ш ГXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXaXX      XXXXX                                                    0 0         d     D       D           D   g   D        D XXXXXXXXXXXXXXXa щ ЕXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXX                               00000000G00000000                   d g           D       g           g        D XXXIXKG G G Gй aXXXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXu P                               0    0    0          0L0         d     g   D   g   D       g                D XXXXXXXXXXXXXXXa ш ГXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXX0  0                                                            d D       D       D   D       D            D  XXXXXXXXXXXXXXaXXXXXXXXXXXXXXXXXXXXXXXX',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXG           0                    0       0           0G0         d D   D   D       D   D   D   D   D      R D                aXXXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXG           0                    0       0           0 0         d D   D   D       D   D   D   D   D      R D                aXXXXXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXX                   0                0        000    000    000  DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXX                                              G0           0K                                      XXXXXXXXXXXXXXXXXXXXXXXXXaXXXXXXXXXXXXXXXXXXXXXXXX',
     'XXXXXXXXXXXXXXXXXXXXXXXXXAXXXX      KKK                           0                 000           000                                      XXXXXXXXXXXXXXXXXXXXXXXXAXXXXXXXXXXXXXXXXXXXXXXXX',
@@ -29,4 +29,4 @@ gold_max = 0
 for el in level_parkour_map:
     for s in el:
         if s in ['G', 'g']:
-            gold_max = 60
+            gold_max += 1
